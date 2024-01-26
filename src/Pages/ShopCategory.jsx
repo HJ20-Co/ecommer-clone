5a1f6757d67d2d921ab5ccdecfc96791202d7025
diep nguyen <diepmbacc@gmail.com>
2024-01-26 22:26:35 +0700

    bai8

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -26,6 +26,9 @@ export const ShopCategory = (props) => {
             }
           })}
         </div>
+        <div className="shopcategory-loadmore">
+          Explore More
+        </div>
     </div>
   )
 }
